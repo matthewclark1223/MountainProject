219ed4df1945c371c963b4cf65d45aa20540f306
Matt Clark <matthewclark989@boisestate.edu>
2019-08-15 15:13:25 -0600

    Added better(rounded) stars category

--- Scripts/Cleaning.R
+++ Scripts/Cleaning.R
@@ -88,8 +88,17 @@ df<-df %>% filter(routes.rating!="XXX")
 df$safety<-ifelse(grepl("PG", df$routes.rating), "PG13",
                   ifelse(grepl("R",df$routes.rating ),"R",
                   ifelse(grepl("X",df$routes.rating ),"X", "Safe")))
-                           
-                                 
+             
+
+              
+#Make a better star category
+df$Stars<-ifelse(df$routes.stars %in% c("4.5","4.6","4.7","4.8","4.9","5"),"5",
+             ifelse(df$routes.stars %in% c("4.4","4.3","4.3","4.2","4.1","4","3.9","3.8","3.7","3.6","3.5"),"4",
+             ifelse(df$routes.stars %in% c("3.4","3.3","3.3","3.2","3.1","3","2.9","2.8","2.7","2.6","2.5"),"3",
+             ifelse(df$routes.stars %in% c("2.4","2.3","2.3","2.2","2.1","2","1.9","1.8","1.7","1.6","1.5"),"2",  
+             ifelse(df$routes.stars %in% c("1.4","1.3","1.3","1.2","1.1","1","0.9","0.8","0.7","0.6","0.5"),"1",
+             ifelse(df$routes.stars %in% c("0.4","0.3","0.3","0.2","0.1","0"),"0","XXX"))))))
+
 #Create a cleaned CSV
 dir.create("~/MtnProject/DataOutput")
 write.csv(df,"~/MtnProject/DataOutput/RouteDataCleaned.csv")
